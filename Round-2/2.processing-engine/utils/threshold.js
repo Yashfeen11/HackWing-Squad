c4b98a5dda778229a8913ef9a85496cac123f266
Yashfeen11 <yashfeen6343@gmail.com>
2026-01-11 17:54:58 +0530

    Update processing engine thresholds config and add Round 2 documentation

--- Round-2/2.processing-engine/utils/threshold.js
+++
@@ -1,9 +0,0 @@
-// utils/thresholds.js
-
-module.exports = {
-  NORMAL_LOSS_PERCENT: 0.05,     // 5%
-  LEAKAGE_LOSS_PERCENT: 0.15,    // 15%
-  PRESSURE_DROP_LIMIT: 0.2,      // 20%
-  MIN_LEAKAGE_DURATION: 30,      // minutes
-  SPIKE_MULTIPLIER: 1.2          // theft spike
-};
